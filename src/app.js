2d9bf1130b8c02f54704632c1c2063f788f9b26b
Cipher-30 <akamitkumar3030@gmail.com>
2024-10-10 11:29:50 +0530

    handle multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,101 @@ const express = require('express');
 
 const app = express();
 
+
+
+
+
+// ------------------------------------------------------
+    //  ************ MULTIPLE ROUTE HANDLERS **************
+
+    app.get( "/person" , (req, res, next) => {
+       
+        console.log("handeling 1 route handler");
+        next(); // will move to send route handler
+        
+        // res.send( "calling response 1") 
+        //once res is send tcp connection is lost
+    }, 
+    (req, res, next) => {
+       
+        console.log("handeling the 2 route handler");
+
+        // res.send( "calling response 2")
+
+        next(); // move to next route handler
+        
+    },
+    (req, res, next) => {
+       
+        console.log("handeling the 3 route handler");
+
+        res.send( "calling response 3")
+
+        // next(); // move to next route handler
+        
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------
+    //  ***************** QUERY PARAMETERS ********************
+
+
+// to get the query param from "/user?userId=101"
+// to get the query param from "/user?userId=101&password=1234"
+app.get( "/user", (req, res) => {
+
+       //will give the "query" from url (userid and pass)
+       console.log(req.query);
+       
+    res.send({firstname:"amit", lastname:"kumar"});
+});
+
+
+// ----------------------------------------------------------
+// ***************** DYNAMIC ROUTES ********************
+ 
+  // /person/007/cypher
+  app.get( "/person/:personId/:personName",  (req, res) => {
+       
+    // will give the dynamic route params (personId , personName)
+     console.log(req.params);
+     
+
+    res.send("sending the dynamic routes");
+  })
+
+
+
+// ----------------------------------------------------------
+
+//  "?" -> optional char
+//   + -> multiple same char
+//   * -> multiple any char
+//   /a/ -> if a there with any other char
+app.get( "/ab*c", (req, res) => {
+    res.send({firstname:"amit", lastname:"kumar"} );
+})
+
+
+
+// --------------------------------------------------------
+
+
+
+
+
+
 //REQUEST HANDLER (DEFAULT ROUTE)
 // app.use( (req,res) => {
 //     res.send("this is respose for server on 3000")
@@ -13,9 +108,9 @@ app.use( "/hello" , (req, res) => {
 });
 
 
-app.get( "/user", (req, res) => {
-    res.send({firstname:"amit", lastname:"kumar"});
-});
+// -------------------------------------------
+
+
 
 
 //CALLING POST API METHOD ON "/user"
